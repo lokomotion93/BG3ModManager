13ed8985330a9d03de91c2526ae25e93776a1e76
LaughingLeader <laughingleader@gmail.com>
2025-10-06 22:11:16 -0500

    Change the mod properties window icon

--- src/GUI/ModManager/Windows/ModPropertiesWindow.axaml.cs
+++ src/GUI/ModManager/Windows/ModPropertiesWindow.axaml.cs
@@ -1,14 +1,53 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Presenters;
+using Avalonia.Svg.Skia;
 
 using Material.Icons;
+using Material.Icons.Avalonia;
 
 using ModManager.Controls;
+using ModManager.Models.Mod;
 using ModManager.ViewModels;
+
+using SkiaSharp;
+
+using Svg.Skia;
 
 namespace ModManager.Windows;
 public partial class ModPropertiesWindow : HideWindowBase<ModPropertiesWindowViewModel>
 {
+	private object? ModToWindowIcon(ModData? mod)
+	{
+		if (mod != null)
+		{
+			if (mod.IsToolkitProject == true || mod.IsLooseMod)
+			{
+				var svgImage = new SvgImage
+				{
+					Css = ".Black { fill: #1098DF; }",
+					Source = SvgSource.Load("avares://ModManager/Assets/Icons/file-cog.svg", baseUri: null),
+				};
+				using var mem = new MemoryStream();
+				svgImage.Source.Picture.ToImage(mem, SKColors.Empty, SKEncodedImageFormat.Png, 100, 1f, 1f, SKColorType.Rgba8888, SKAlphaType.Unpremul, SKColorSpace.CreateSrgb());
+				mem.Position = 0;
+				return new WindowIcon(mem);
+			}
+			else
+			{
+				var svgImage = new SvgImage
+				{
+					Css = ".Black { fill: #1098DF; }",
+					Source = SvgSource.Load("avares://ModManager/Assets/Icons/folder-cog.svg", baseUri: null)
+				};
+				using var mem = new MemoryStream();
+				svgImage.Source.Picture.ToImage(mem, SKColors.Empty, SKEncodedImageFormat.Png, 100, 1f, 1f, SKColorType.Rgba8888, SKAlphaType.Unpremul, SKColorSpace.CreateSrgb());
+				mem.Position = 0;
+				return new WindowIcon(mem);
+			}
+		}
+		return new WindowIcon("avares://ModManager/Assets/BG3ModManager.ico");
+	}
+
 	public ModPropertiesWindow()
 	{
 		InitializeComponent();
@@ -26,6 +65,8 @@ public partial class ModPropertiesWindow : HideWindowBase<ModPropertiesWindowVie
 
 				this.OneWayBind(ViewModel, x => x.IsEditorMod, x => x.ModTypeIconControl.Kind,
 					b => b ? MaterialIconKind.Folder : MaterialIconKind.File);
+
+				//this.OneWayBind(ViewModel, x => x.Mod, x => x.Icon, ModToWindowIcon);
 
 				ViewModel.OKCommand.Subscribe(x => Hide());
 				ViewModel.CancelCommand.Subscribe(x => Hide());
