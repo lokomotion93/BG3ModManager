b763e6b9c54f47937dbb8b722086621bfeac4cce
LaughingLeader <laughingleader@gmail.com>
2026-03-03 20:24:53 -0600

    Add a tintable image control

---
+++ src/GUI/ModManager/Controls/TintImage.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Media;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModManager.Controls;
+
+public class TintImage : ContentControl
+{
+	public static readonly StyledProperty<IImage?> SourceProperty = Image.SourceProperty.AddOwner<TintImage>();
+
+	public IImage? Source
+	{
+		get => GetValue(SourceProperty);
+		set => SetValue(SourceProperty, value);
+	}
+}
